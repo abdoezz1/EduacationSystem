df12fc8fade89620e4fce930d8de6c80f69dfa1e
abdoezz1 <133367786+abdoezz1@users.noreply.github.com>
2024-10-02 18:37:28 +0300

    Add files via upload

---
+++ Validators/UniqueCourseAttribute.cs
@@ -0,0 +1,21 @@
+﻿using MVC_day1.Models;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace MVC_day1.Validators
+{
+	public class UniqueCourseAttribute :ValidationAttribute
+	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) 
+		{
+            Intelligent db = new Intelligent();
+			var name  = value as string;
+			var course = db.Courses.FirstOrDefault(c => c.Name == name);
+			if (course == null) 
+			{
+				return ValidationResult.Success;
+			}
+			return new ValidationResult("Department Already exists !");
+		}
+	}
+}
---
+++ Validators/UniqueDeptAttribute.cs
@@ -0,0 +1,20 @@
+﻿using MVC_day1.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_day1.Validators
+{
+	public class UniqueDeptAttribute :ValidationAttribute
+	{
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+            Intelligent db = new Intelligent();
+			var name = value as string;
+			var dept = db.Departments.FirstOrDefault(x => x.Name == name);
+			if (dept == null) 
+			{
+				return ValidationResult.Success;
+			}
+			return new ValidationResult("Department Already exists !!");
+		}
+	}
+}
